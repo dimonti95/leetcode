833747c014f2b79f51f4ae2619c16a2ae2f2c34d
dimonti95 <dimonti94@gmail.com>
2021-09-02 00:37:43 -0400

    1971: implementation & explanation

---
+++ Easy/FindIfPathExistsInGraph.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+ var validPath = function(n, edges, start, end) {
+    
+  let graph = buildGraph(edges);
+  
+  // dfs
+  let stack = [ start ];
+  let visited = new Set();
+
+  while(stack.length > 0) {
+      let curr = stack.pop();
+      if (visited.has(curr)) continue;
+      visited.add(curr);
+      if (curr === end) return true;
+  
+      graph[curr].forEach(neighbor => {
+          stack.push(neighbor);
+      });
+  }
+  
+  return false;
+  
+};
+
+let buildGraph = function (edges) {
+  const graph = {};
+  
+  for (let edge of edges) {
+      const [a, b]  = edge;
+      if (!(a in graph)) graph[a] = [];
+      if (!(b in graph)) graph[b] = [];
+      graph[a].push(b);
+      graph[b].push(a);
+  }
+  
+  return graph;
+};
+
+/* 
+
+  Explanation:
+
+  Convert edge list to an adjacency list then do iterative DFS
+
+  Time Complexity: O(n^2)
+  Space Complexity: O(n)
+  
+  where n is the number of nodes
+
+*/
