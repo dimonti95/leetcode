1f4b6f02ca142b716cb18593b4942d5496df703c
dimonti95 <dimonti94@gmail.com>
2021-09-04 17:02:25 -0400

    70: Climbing Stairs - implementation & explanation

---
+++ Easy/ClimbingStairs.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {  
+  let map = {};
+  
+  let recurse = function (x) {
+      if (map[x]) return map[x];
+      if (x >= n) return x === n ? 1 : 0;
+      
+      map[x] = recurse(x + 1) + recurse(x + 2);
+      return map[x];
+  };
+  
+  return recurse(0);
+};
+
+/*
+
+  Explanation:
+  
+  Recursive memoized DP solution
+  
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+
+*/
