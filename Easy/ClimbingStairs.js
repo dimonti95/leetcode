1c630cc8cfab02d6ab9eaf955d510ba226b7b3fd
dimonti95 <dimonti94@gmail.com>
2021-09-05 19:53:53 -0400

    bottoms up solution for the climbing stairs problem

--- Easy/ClimbingStairs.js
+++ Easy/ClimbingStairs.js
@@ -28,3 +28,36 @@
   where n is the number of steps
 
 */
+
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {  
+    
+  let n1 = 0;
+  let n2 = 1;
+  
+  while (n > 0) {
+      let temp = n2;
+      n2 = n1 + n2;
+      n1 = temp;
+      n--;
+  }
+  
+  return n2;
+};
+
+/*
+  Explanation:
+  
+  Bottoms up DP solution.
+  
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+  
+  where n is the number of stairs
+  
+*/
