4dd67a40016a0c53992ed2d9e0101dc0c7ddb782
dimonti95 <dimonti94@gmail.com>
2021-09-13 19:45:29 -0400

    746: Min Cost Climbing Stairs - iterative

--- Easy/MinCostClimbingStairs.js
+++ Easy/MinCostClimbingStairs.js
@@ -2,6 +2,45 @@
  * @param {number[]} cost
  * @return {number}
  */
+ var minCostClimbingStairs = function(cost) { 
+    
+  let min1 = 0;
+  let min2 = 0;
+  for (let i = 0; i < cost.length; i++) {
+      let current = cost[i] + Math.min(min1, min2);
+      min1 = min2
+      min2 = current;
+  }
+  
+  return Math.min(min1, min2);
+};
+
+/*
+          
+  Iterative bottom-up
+  
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+  
+  where n is the size of the cost array
+  
+  Example: [0, 2, 2, 1]
+  
+  1.  current = 0 + min(0,0)  min1 = 0, min2 = 0
+  2.  current = 2 + min(0,0)  min1 = 0, min2 = 0
+  3.  current = 2 + min(0,2)  min1 = 0, min2 = 2
+  4.  current = 1 + min(2,2)  min1 = 2, min2 = 3
+  return min(2, 3)
+  = 2
+  
+*/
+
+
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
 var minCostClimbingStairs = function(cost) {
  
     let mem = {};
@@ -26,5 +65,7 @@ var minCostClimbingStairs = function(cost) {
     
     Time Complexity: O(n)
     Space Complexity: O(n)
+
+    where n is the size of the cost array
 
 */
