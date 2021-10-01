5d7604f283e78acc4092dc8a759c0e645f179a1d
dimonti95 <dimonti94@gmail.com>
2021-09-30 22:36:34 -0400

    997: Find the Town Judge optimal

--- Easy/FindTheTownJudge.js
+++ Easy/FindTheTownJudge.js
@@ -5,6 +5,41 @@
  */
  var findJudge = function(n, trust) {
     
+  let count = {};
+  
+  for (let edge of trust) {
+      count[edge[0]] ? count[edge[0]]-- : count[edge[0]] = -1;
+      count[edge[1]] ? count[edge[1]]++ : count[edge[1]] = 1;
+  }
+  
+  for (let i = 1; i <= n; i++) {
+      if (count[i] === n - 1) return i;
+  }
+  
+  return n === 1 ? 1 : -1;
+};
+
+/*
+
+  Create a map of each person to the number of people who trust them - the number of people who that person trusts.
+  The judge is the person mapped to n - 1 people.
+  
+  Time Complexity: O(n)
+  Space Complexity: O(m)
+  
+  Where n is the number of edges (trust) and m is the number of nodes (people)
+
+*/
+
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+ var findJudge = function(n, trust) {
+    
   // build directed adjacency list
   let adjList = {};
   for (let i = 0; i < trust.length; i++) {
