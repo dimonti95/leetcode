050936b9b9206699fc66de6a7de15f7bb2b6268f
dimonti95 <dimonti94@gmail.com>
2021-10-01 13:37:20 -0400

    559: Maximum Depth of N-ary Tree - BFS

--- Easy/MaximumDepthOfN-aryTree.js
+++ Easy/MaximumDepthOfN-aryTree.js
@@ -43,3 +43,51 @@
   Where n is the number of nodes in the tree and h is the height of the tree.
 
 */
+
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    
+  if (root === null) return 0;
+  
+  let queue = [ root ];
+  let depth = 0;
+  
+  while (queue.length > 0) {
+      let size = queue.length;
+      
+      for (let i = 0; i < size; i++) {
+          let current = queue.shift();
+          for (let child of current.children) {
+              queue.push(child);
+          }
+      }
+      depth++;
+  }
+  
+  return depth;
+  
+};
+
+/*
+
+  Traverse the tree keeping track of the current depth (Breadth First Search).
+  
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+  
+  Where n is the number of nodes in the tree.
+
+*/
