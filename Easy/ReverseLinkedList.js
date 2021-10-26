453020df653cf18bf8de514b17918898d99b1461
dimonti95 <dimonti94@gmail.com>
2021-10-26 19:49:56 -0400

    206: Reverse Linked List

--- Easy/ReverseLinkedList.js
+++ Easy/ReverseLinkedList.js
@@ -35,3 +35,38 @@
   Where n is the number of nodes in the list.
 
 */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    
+  if (head === null) return null;
+  
+  let temp = head;
+  
+  if (head.next) {
+      temp = reverseList(head.next);
+      head.next.next = head;
+  }
+  
+  head.next = null;
+return temp;
+  
+};
+
+/*
+
+  Recursive
+
+*/
