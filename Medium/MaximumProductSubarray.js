be6d7ba220e061c373ee114bc13563b60ace5660
Nick DiMonti <dimonti94@gmail.com>
2021-11-06 21:24:50 -0400

    152: Maximum Product Subarray

--- Medium/MaximumProductSubarray.js
+++ Medium/MaximumProductSubarray.js
@@ -4,28 +4,34 @@
  */
  var maxProduct = function(nums) {
     
-    let max = 1;
-    let min = 1;
+    let currMax = 1;
+    let currMin = 1;
     let maxProduct = Math.max(...nums);
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            max = 1;
-            min = 1;
-            continue;
-        }
-        let temp = max;
-        max = Math.max(nums[i] * max, nums[i] * min, nums[i]);
-        min = Math.min(nums[i] * temp, nums[i] * min, nums[i]);
-        maxProduct = Math.max(maxProduct, max);
-    }
     
+    for (let i = 0; i < nums.length; i++) {
+        let temp = currMax;
+        currMax = Math.max(nums[i], currMax * nums[i], currMin * nums[i]);
+        currMin = Math.min(nums[i], temp * nums[i], currMin * nums[i]);
+        maxProduct = Math.max(maxProduct, currMax);
+    }
+
     return maxProduct;
 };
 
 /*
 
     Track current max and min products at each index (to deal with negatives)
+
+
+    Example: [2,3,-2,4]
+    0: [2, 2]
+    1: [2, 6]
+    2: [-12, -2]
+    3: [-48, 4]
+
+    i: [currMin, currMax]
     
+
     Time Complexity: O(n)
     Space Complexity: O(1)
     
