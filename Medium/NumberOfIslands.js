d620c183cc5c4e0c353977d9e77b7fbe7879fc7e
Nick DiMonti <dimonti94@gmail.com>
2021-11-12 23:52:09 -0500

    200: Number of Islands

--- Medium/NumberOfIslands.js
+++ Medium/NumberOfIslands.js
@@ -4,6 +4,51 @@
  */
  var numIslands = function(grid) {
     
+    let visited = new Set();
+    let numOfIslands = 0;
+    let nRows = grid.length;
+    let nCols = grid[0].length;
+    
+    let dfs = function (m, n) {
+        let cell = [m, n].toString();
+        if (visited.has(cell) || grid[m] === undefined || grid[m][n] !== "1") return;
+        visited.add(cell);
+        
+        dfs(m + 1, n);
+        dfs(m - 1, n);
+        dfs(m, n + 1);
+        dfs(m, n - 1);
+    }
+    
+    for (let m = 0; m < nRows; m++) {
+        for (let n = 0; n < nCols; n++) {
+            if (grid[m][n] === "1" && !visited.has([m,n].toString())) {
+                dfs(m, n);
+                numOfIslands += 1;
+            }
+        }
+    }
+    
+    return numOfIslands;
+};
+
+/*
+
+    DFS on every land node that hasn't already been visited
+    
+    Time Complexity: O(m*n)
+    Space Complexity: O(m*n)
+
+*/
+
+
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+ var numIslands = function(grid) {
+    
     if (grid.length === 0) return 0;
     
     let nRows = grid.length;
