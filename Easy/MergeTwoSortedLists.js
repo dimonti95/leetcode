ad9fbb72323b2c851f0dada9136a92942cd41a9e
Nick DiMonti <dimonti94@gmail.com>
2021-11-17 21:20:35 -0500

    21: Merge Two Sorted Lists

---
+++ Easy/MergeTwoSortedLists.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(l1, l2) {
+    
+    let root = new ListNode();
+    let current = root;
+    
+    while (l1 && l2) {
+        
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        }
+        else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        
+        current = current.next;
+    }
+    
+    current.next = l1 || l2;
+    
+    return root.next;    
+};
+
+/*
+    
+    Time: O(n)
+    Space: O(1)
+
+*/
