14b56e66bee78adfc27524db11f5d0c06ba8ecd8
Nick DiMonti <dimonti94@gmail.com>
2021-11-27 22:09:17 -0500

    100: Same Tree

--- Easy/SameTree.js
+++ Easy/SameTree.js
@@ -35,3 +35,50 @@
   Where n is the number of nodes and h is longest common height between both trees
 
 */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+    
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  
+  let pStack = [ p ];
+  let qStack = [ q ];
+  
+  while (pStack.length > 0 && qStack.length > 0) {
+      
+      let pCurrent = pStack.pop();
+      let qCurrent = qStack.pop();
+      
+      if (pCurrent === null && qCurrent === null) continue;
+      if (pCurrent === null || qCurrent === null || pCurrent.val !== qCurrent.val) return false;
+      
+      pStack.push(pCurrent.left);
+      qStack.push(qCurrent.left);
+      
+      pStack.push(pCurrent.right);
+      qStack.push(qCurrent.right);
+  }
+  
+  return true;
+};
+
+/*
+  
+  Iterative
+
+*/
