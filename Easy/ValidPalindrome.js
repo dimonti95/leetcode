f1ed2529722bdcf2c1bb79771ea7d16f8652799b
Nick DiMonti <dimonti94@gmail.com>
2022-06-24 20:22:04 -0400

    125: Valid Palindrome

--- Easy/ValidPalindrome.js
+++ Easy/ValidPalindrome.js
@@ -4,26 +4,29 @@
  */
  var isPalindrome = function(s) {
     
+  let left = 0;
   let right = s.length - 1;
-  for (let left = 0; left < s.length; left++) {
-      if (!isAlphanumeric(s.charAt(left))) continue;
-      while (!isAlphanumeric(s.charAt(right))) right--;
-      if (left >= right) break;
-      if (s.charAt(left).toLowerCase() !== s.charAt(right).toLowerCase()) 
+  
+  while(left < right) {
+      
+      while(left < right && !isAlphanumeric(s.charAt(left)))
+          left++;
+      while(left < right && !isAlphanumeric(s.charAt(right)))
+          right--;
+      
+      if (s.charAt(left).toLowerCase() !== s.charAt(right).toLowerCase())
           return false;
+      
+      left++;
       right--;
   }
   
   return true;
 };
 
-const isAlphanumeric = function (char) {
-  return /[0-9a-zA-Z]/.test(char);
-}
-
 /*
 
-  Two pointers on each end ignoring case and non-alphanumeric characters
+  Two pointers on each end ignoring case and non-alphanumerics
 
   Time Complexity: O(n)
   Space Complexity: O(1)
