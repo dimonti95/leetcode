dd99e6baf9838eb3ac6a5c048bcf3416d0c029e0
Nick DiMonti <dimonti94@gmail.com>
2022-06-26 19:27:57 -0400

    424: Longest Repeating Character Replacement

--- Medium/LongestRepeatingCharacterReplacement.js
+++ Medium/LongestRepeatingCharacterReplacement.js
@@ -12,12 +12,12 @@
   for (let right = 0; right < s.length; right++) {
     map[s.charAt(right)] ? map[s.charAt(right)]++ : map[s.charAt(right)] = 1;
     let count = (right - left + 1) - getCountOfMostFrequentChar(map);
-      while (count > k) {
-          map[s.charAt(left)]--;
-          left++;
-          count = (right - left + 1) - getCountOfMostFrequentChar(map);
-      }
-      max = Math.max(max, right - left + 1);
+    while (count > k) {
+      map[s.charAt(left)]--;
+      left++;
+      count = (right - left + 1) - getCountOfMostFrequentChar(map);
+    }
+    max = Math.max(max, right - left + 1);
   }
   
   return max; 
@@ -27,7 +27,7 @@
 let getCountOfMostFrequentChar = function (map) {
   let max = 0;
   for (let key in map) {
-      if (map[key] > max) max = map[key];
+    max = Math.max(max, map[key]);
   }
   return max;
 }
@@ -37,7 +37,7 @@ let getCountOfMostFrequentChar = function (map) {
   Two pointers, sliding window, track (count of most frequent character - size of window)
 
   Time Complexity: O(n)
-  Space Complexity: O(1) (at most 26 characters will be mapped)
+  Space Complexity: O(1) (at most 26 characters get mapped)
 
   Where n is the length of the string
 
