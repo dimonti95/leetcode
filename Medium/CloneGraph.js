bca82ecfed3a474e12286b7b8e4f72f6678c103f
Nick DiMonti <dimonti94@gmail.com>
2022-07-30 21:02:20 -0400

    133: Clone Graph

--- Medium/CloneGraph.js
+++ Medium/CloneGraph.js
@@ -42,3 +42,56 @@
     Space Compelxity: O(v)
 
 */
+
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+  
+  if (!node)
+    return null;
+  
+  let map = {};
+  let stack = [ node ];
+  let visited = new Set([ node.val ]);
+  map[node.val] = new Node(node.val);
+  
+  while (stack.length > 0) {
+    let current = stack.pop();
+    let copy = map[current.val];
+    
+    for (let neighbor of current.neighbors) {
+      if (!map[neighbor.val])
+        map[neighbor.val] = new Node(neighbor.val);
+      let temp = map[neighbor.val];
+      copy.neighbors.push(temp);
+      if (!visited.has(neighbor.val)) {
+        stack.push(neighbor);
+        visited.add(neighbor.val);
+      }
+    }
+  }
+  
+  return map[node.val];
+};
+
+ /*
+	
+	Iterative DFS
+  
+  	Time: O(v + e)
+	Space: O(v)
+
+  */
+
