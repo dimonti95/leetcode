839fb7808cdcf858865b5605673a0e2325cc8d0f
Nick DiMonti <dimonti94@gmail.com>
2022-08-10 20:17:37 -0400

    57: Insert Interval

---
+++ Medium/InsertInterval.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+ var insert = function(intervals, newInterval) {
+    let res = [];
+    
+    for (let i = 0; i < intervals.length; i++) {
+      if (newInterval[1] < intervals[i][0]) {
+        res.push(newInterval); 
+        return res.concat(intervals.slice(i));
+      }
+      else if (newInterval[0] > intervals[i][1]) {
+        res.push(intervals[i]);
+      }
+      else {
+        // overlap
+        let start = Math.min(newInterval[0], intervals[i][0]);
+        let end = Math.max(newInterval[1], intervals[i][1]);
+        newInterval = [start, end];
+      } 
+    }
+    
+    res.push(newInterval);
+    
+    return res;
+  };
+  
+  /*
+  
+    Time: O(n)
+    Space: O(n) 
+  
+  */
+ 
