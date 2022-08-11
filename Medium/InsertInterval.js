174e571ac33dbcfe8729bfb023a71dab5b4626e2
Nick DiMonti <dimonti94@gmail.com>
2022-08-10 20:17:54 -0400

    57: Insert Interval

--- Medium/InsertInterval.js
+++ Medium/InsertInterval.js
@@ -3,34 +3,33 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
- var insert = function(intervals, newInterval) {
-    let res = [];
-    
-    for (let i = 0; i < intervals.length; i++) {
-      if (newInterval[1] < intervals[i][0]) {
-        res.push(newInterval); 
-        return res.concat(intervals.slice(i));
-      }
-      else if (newInterval[0] > intervals[i][1]) {
-        res.push(intervals[i]);
-      }
-      else {
-        // overlap
-        let start = Math.min(newInterval[0], intervals[i][0]);
-        let end = Math.max(newInterval[1], intervals[i][1]);
-        newInterval = [start, end];
-      } 
+var insert = function(intervals, newInterval) {
+  let res = [];
+  
+  for (let i = 0; i < intervals.length; i++) {
+    if (newInterval[1] < intervals[i][0]) {
+      res.push(newInterval); 
+      return res.concat(intervals.slice(i));
     }
-    
-    res.push(newInterval);
-    
-    return res;
-  };
+    else if (newInterval[0] > intervals[i][1]) {
+      res.push(intervals[i]);
+    }
+    else {
+      // overlap
+      let start = Math.min(newInterval[0], intervals[i][0]);
+      let end = Math.max(newInterval[1], intervals[i][1]);
+      newInterval = [start, end];
+    } 
+  }
   
-  /*
+  res.push(newInterval);
   
-    Time: O(n)
-    Space: O(n) 
-  
-  */
- 
+  return res;
+};
+
+/*
+
+  Time: O(n)
+  Space: O(n) 
+
+*/
