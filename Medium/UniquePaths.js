09a14cc050ebeb2cb2a1736e89da753fd954d05e
Nick DiMonti <dimonti94@gmail.com>
2022-08-20 19:50:36 -0400

    62: Unique Paths

---
+++ Medium/UniquePaths.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n, mem = {}) {
+  let key = [m,n].toString();
+  if (key in mem)
+    return mem[key];
+  if (m === 0 || n === 0)
+    return 0;
+  if (m === 1 && n === 1)
+    return 1;
+  
+  mem[key] = uniquePaths(m - 1, n, mem) + uniquePaths(m, n - 1, mem);
+  return mem[key];
+};
+
+/*
+
+  Non-memoized
+  Time: O(2^n+m)
+  Space: O(n+m)
+
+  Memoized
+  Time: O(n)
+  Space: O(n+m)
+  
+*/
