66f558d0abe7c1107b937408dfb83ea76b67db9d
Nick DiMonti <dimonti94@gmail.com>
2022-08-27 19:31:28 -0400

    55: Jump Game

--- Medium/JumpGame.js
+++ Medium/JumpGame.js
@@ -60,10 +60,72 @@ var canJump = function(nums, i = 0, mem = {}) {
 
   Top-Down Memoized DP
   
-  Time: O(n*m)
+  Time: O(n^2)
   Space: O(n)
   
   n = length of nums
   m = the average value at each index
+
+  Example: [5,4,3,2,1,0,3] to understand why memoized top down time complexity is O(n*n).
+
+*/
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  
+  let table = new Array(nums.length).fill(false);
+  table[nums.length - 1] = true;
+  
+  for (let i = nums.length - 2; i >= 0; i--) {
+    for (let j = 1; j <= nums[i]; j++) {
+      if (table[i + j]) {
+        table[i] = true;
+        break;
+      }
+    }
+  }
+  
+  return table[0];
+};
+
+/*
+
+  Bottom-up DP (Tabulation)
+  
+  Time: O(n^2)
+  Space: O(n)
+
+  Example: [5,4,3,2,1,0,3] to understand why memoized top down time complexity is O(n*n).
+
+*/
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let lastPos = nums.length - 1;
+  
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (i + nums[i] >= lastPos)
+      lastPos = i;
+  }
+  
+  return lastPos == 0;
+};
+
+/*
+
+  Greedy
+  
+  Time: O(n)
+  Space: O(1)
 
 */
