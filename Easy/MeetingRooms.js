aab6665ab66781d265d6c00aa8f24b23287ea2a9
Nick DiMonti <dimonti94@gmail.com>
2022-09-08 20:07:21 -0400

    252: Meeting Rooms

---
+++ Easy/MeetingRooms.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+  
+  intervals.sort((a,b) => a[0] - b[0]);
+  
+  for (let i = 1; i < intervals.length; i++) {
+    let interval1 = intervals[i - 1];
+    let interval2 = intervals[i];
+    if (interval1[1] > interval2[0])
+      return false;
+  }
+    
+  return true;
+};
+
+/*
+
+  Sort based on start value and check if the end value exceeds the start of the next interval
+
+  Time: O(nlogn)
+  Space: O(1)
+
+*/
