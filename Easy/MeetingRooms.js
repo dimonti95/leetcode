263c25de73ece6244d0b6d1ed2781c4ce9ce1f45
Nick DiMonti <dimonti94@gmail.com>
2022-09-09 20:44:19 -0400

    252: Meeting Rooms

--- Easy/MeetingRooms.js
+++ Easy/MeetingRooms.js
@@ -4,6 +4,39 @@
  */
 var canAttendMeetings = function(intervals) {
   
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = i + 1; j < intervals.length; j++) {
+      let minEnd = Math.min(intervals[i][1], intervals[j][1]);
+      let maxStart = Math.max(intervals[i][0], intervals[j][0]);
+      if (minEnd > maxStart)
+        return false;
+    }
+  }
+  
+  return true;
+};
+
+/*
+
+  Approach 1:
+  
+  Brute force nested for loop
+  
+  Check for overlap between two non-sorted intervals by comparing the minimum end time with the max start time
+  
+  Time: O(n^2)
+  Space: O(1)
+
+*/
+
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+  
   intervals.sort((a,b) => a[0] - b[0]);
   
   for (let i = 1; i < intervals.length; i++) {
