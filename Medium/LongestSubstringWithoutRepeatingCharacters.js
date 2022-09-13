fbd9b5f2b48d6f8e0ee70c8735d95a435aef9763
Nick DiMonti <dimonti94@gmail.com>
2022-09-12 20:06:47 -0400

    3: Longest Substring Without Repeating Characters

--- Medium/LongestSubstringWithoutRepeatingCharacters.js
+++ Medium/LongestSubstringWithoutRepeatingCharacters.js
@@ -25,8 +25,45 @@
   Sliding window algorithm with two pointers
   
   Time Complexity: O(n)
-  Space Complexity: O(n)
+  Space Complexity: O(min(m,n))
   
   Where n is the length of the string
+
+  The size of the Set is upper bounded by the size of the string (n) and the number of unique characters.
+
+*/
+
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  
+  let indexMap = {};
+  let max = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    let c = s.charAt(right);
+    if (s.charAt(right) in indexMap) {
+      left = Math.max(indexMap[s.charAt(right)] + 1, left);
+    }
+    indexMap[s.charAt(right)] = right;
+    max = Math.max(max, right - left + 1);
+  }
+  
+  return max;
+};
+
+
+/*
+
+  Optimized Sliding window
+
+  Time: O(n)
+  Space: O(min(m,n))
+  
+  The size of the Map is upper bounded by the size of the string (n) and the number of unique characters.
 
 */
