079cfd1c61248e630dc94a40dbc1e0d8f4b4fcf1
Nick  DiMonti <nickdimonti@Nicks-MacBook-Pro-3.local>
2024-11-28 10:29:23 -0500

    217. Contains Duplicate

---
+++ C#/Easy/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (int num in nums)
+        {
+            if (set.Contains(num))
+                return true;
+            set.Add(num);
+        }
+
+        return false;
+    }
+}
+
+/*
+
+  Keep a set of unique numbers and return false on duplicates
+  
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+
+  Where n is the size of the array    
+
+*/
