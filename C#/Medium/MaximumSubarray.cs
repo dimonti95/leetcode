638b5fb677f8c3078d3b8fb622e1db3bd6b38559
Nick  DiMonti <nickdimonti@Nicks-MacBook-Pro-3.local>
2024-11-28 11:47:29 -0500

    53. Maximum Subarray

---
+++ C#/Medium/MaximumSubarray.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int max = nums[0];
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            currentSum = Math.Max(nums[i], currentSum + nums[i]);
+            max = Math.Max(max, currentSum);
+        }
+
+        return max;
+    }
+}
+
+/* 
+
+  Explanation:
+
+  Loop over the array keeping a current sum to be either the current value or the current sum up to that point
+  + the current value, whichever is larger. Then check for a new max.
+
+  (Kadane's Algorithm)
+
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+
+*/
