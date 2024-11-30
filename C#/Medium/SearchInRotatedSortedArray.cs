a723566ad2ba5af8b7aa8b0cb5274c61ca595f84
Nick  DiMonti <dimonti94@gmail.com>
2024-11-30 12:12:43 -0500

    33. Search in Rotated Sorted Array

---
+++ C#/Medium/SearchInRotatedSortedArray.cs
@@ -0,0 +1,52 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+        int middle = (int)Math.Floor((double)nums.Length / 2);
+
+        while (left <= right)
+        {
+            if (nums[middle] == target)
+            {
+                return middle;
+            }
+            else if (nums[left] <= nums[middle])
+            {
+                if (target >= nums[left] && target < nums[middle])
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            else if (nums[right] >= nums[middle])
+            {
+                if (target > nums[middle] && target <= nums[right])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            middle = (int)Math.Floor((double)(right + left) / 2);
+        }
+
+        return -1;
+    }
+}
+
+/*
+
+    Binary Search with left and right pointers
+    (1) Figure out which part of the sorted array the middle is in
+    (2) Check if target is in that range
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+
+*/
