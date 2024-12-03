29c80f92c5fc3aa6f55b6bbfb77bf95863e90987
Nick  DiMonti <dimonti94@gmail.com>
2024-12-03 13:30:56 -0500

    206. Reverse Linked List

---
+++ C#/Easy/ReverseLinkedList.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        
+        if (head == null) return null;
+
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null)
+        {
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return previous;
+    }
+}
+
+/*
+
+  
+    Loop over the list and reverse the pointer of each node.
+
+    Space Complexity: O(1)
+    Time Complexity: O(n)
+  
+    Where n is the number of nodes in the list.
+
+*/
