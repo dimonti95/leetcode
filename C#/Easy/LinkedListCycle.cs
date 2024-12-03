6ceace90d9d3ed2e61cdfb0c5a2411713fccef9d
Nick  DiMonti <dimonti94@gmail.com>
2024-12-03 15:44:31 -0500

    141. Linked List Cycle

---
+++ C#/Easy/LinkedListCycle.cs
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null)
+        {
+            if (fast.next == null) return false;
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
+
+/*
+
+    Brute force solution: 
+    * Iterate through the list
+    * Use a a HashSet to find cycle
+
+    Time: O(n)
+    Space: O(n)
+
+    Where n is the number of nodes in the list
+
+    ---------------------------------------------
+
+    Optimized space solution:
+    * Iterate through the list with a slow and fast pointer
+    * If the pointers meet then there's a cycle
+
+    Time: O(n)
+    Space: O(1)
+
+*/
