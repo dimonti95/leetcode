b1925c2ec8e4805c54e03d7816f21706a00df682
Nick  DiMonti <dimonti94@gmail.com>
2024-12-03 14:47:26 -0500

    206. Reverse Linked List

--- C#/Easy/ReverseLinkedList.cs
+++ C#/Easy/ReverseLinkedList.cs
@@ -39,3 +39,43 @@ public class Solution {
     Where n is the number of nodes in the list.
 
 */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        
+        if (head == null) return null;
+
+        ListNode temp = head;
+
+        if (head.next != null)
+        {
+            temp = ReverseList(head.next);
+            head.next.next = head;
+        }
+
+        head.next = null;
+        return temp;
+    }
+}
+
+/*
+
+    Recursive
+
+    Time: O(n)
+    Space: O(n)
+
+*/
