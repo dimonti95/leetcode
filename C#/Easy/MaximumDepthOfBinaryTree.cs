84a2cd84c1287027a409a9ea17b63290f483a81e
Nick  DiMonti <dimonti94@gmail.com>
2024-12-05 12:34:18 -0500

    104. Maximum Depth of Binary Tree

--- C#/Easy/MaximumDepthOfBinaryTree.cs
+++ C#/Easy/MaximumDepthOfBinaryTree.cs
@@ -32,3 +32,52 @@ public class Solution {
   Where n is the number of nodes in the tree and h is the max height
 
 */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        
+        Stack<Tuple<TreeNode, int>> stack = new Stack<Tuple<TreeNode, int>>();
+        if (root != null) stack.Push(new Tuple<TreeNode, int>(root, 1));
+        Tuple<TreeNode, int> current = null;
+        int max = 0;
+
+        while (stack.Count > 0)
+        {
+            current = stack.Pop();
+            max = Math.Max(max, current.Item2);
+
+            if (current.Item1.left != null)
+                stack.Push(new Tuple<TreeNode, int>(current.Item1.left, current.Item2 + 1));
+            if (current.Item1.right != null)
+                stack.Push(new Tuple<TreeNode, int>(current.Item1.right, current.Item2 + 1));
+        }
+
+        return max;
+    }
+}
+
+/*
+
+  Iterative DFS
+  
+  Time Complexity: O(n)
+  Space Complexity: O(h)
+  
+  Where n is the number of nodes in the tree and h is the max height
+
+*/
