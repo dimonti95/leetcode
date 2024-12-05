42df226c944b409123baa3d6ba82eeaa4e173936
Nick  DiMonti <dimonti94@gmail.com>
2024-12-05 17:58:34 -0500

    100. Same Tree

--- C#/Easy/SameTree.cs
+++ C#/Easy/SameTree.cs
@@ -37,3 +37,82 @@ public class Solution {
     Where n is the number of nodes and h is longest common height between both trees
 
 */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        var pStack = new Stack<TreeNode>();
+        pStack.Push(p);
+
+        var qStack = new Stack<TreeNode>();
+        qStack.Push(q);
+
+        while (pStack.Count > 0 && qStack.Count > 0)
+        {
+            TreeNode pCurrent = pStack.Pop();
+            TreeNode qCurrent = qStack.Pop();
+            if (pCurrent.val != qCurrent.val) return false;
+
+            // left
+            if (pCurrent.left != null && qCurrent.left != null)
+            {
+                pStack.Push(pCurrent.left);
+                qStack.Push(qCurrent.left);
+            }
+            else if (pCurrent.left == null && qCurrent.left == null)
+            {
+                // no-op
+            }
+            else
+            {
+                return false;
+            }
+
+            // right
+            if (pCurrent.right != null && qCurrent.right != null)
+            {
+                pStack.Push(pCurrent.right);
+                qStack.Push(qCurrent.right);
+            }
+            else if (pCurrent.right == null && qCurrent.right == null)
+            {
+                // no-op
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+
+    Iterative DFS
+
+    Time: O(n)
+    Space: O(h)
+    
+    Where n is the number of nodes and h is longest common height between both trees
+
+*/
