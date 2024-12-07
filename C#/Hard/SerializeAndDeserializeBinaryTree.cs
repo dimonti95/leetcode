7010eda6ff363c6450cb1e79c4920cd9426045ac
Nick  DiMonti <dimonti94@gmail.com>
2024-12-07 15:36:59 -0500

    297. Serialize and Deserialize Binary Tree

---
+++ C#/Hard/SerializeAndDeserializeBinaryTree.cs
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public string serialize(TreeNode root)
+    {
+        var sb = new StringBuilder();
+
+        void serializeDfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                sb.Append("N" + ",");
+                return;
+            } 
+            
+            sb.Append(node.val + ",");
+            serializeDfs(node.left);
+            serializeDfs(node.right);
+        }
+
+        serializeDfs(root);
+        Console.WriteLine(sb.ToString());
+        return sb.ToString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(string data)
+    {
+        string[] vals = data.Split(',');
+        int i = 0;
+
+        TreeNode dfs(string[] vals)
+        {
+            string val = vals[i];
+            if (val == "N")
+            {
+                i += 1;
+                return null;
+            }
+            
+
+            var node = new TreeNode();
+            node.val = int.Parse(val);
+            i += 1;
+
+            node.left = dfs(vals);
+            node.right = dfs(vals);
+            
+            return node;
+        }
+
+        return dfs(vals);
+    }
+}
+
+/*
+
+    Serialize
+    Time: O(n)
+    Space: O(n)
+
+    Deserialize
+    Time: O(n)
+    Space: O(n)
+
+    Where n is the number of nodes in the tree
+
+*/
