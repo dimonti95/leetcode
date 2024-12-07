c03667cb9f185039a15148df2991f7ed51796951
Nick  DiMonti <dimonti94@gmail.com>
2024-12-06 20:26:16 -0500

    230. Kth Smallest Element in a BST

---
+++ C#/Medium/KthSmallestElementInABST.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int KthSmallest(TreeNode root, int k)
+    {
+        var stack = new Stack<TreeNode>();
+        TreeNode current = root;
+
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+
+            // in-order
+            if (k == 1) return current.val;
+            else k--;
+
+            current = current.right;
+        }
+
+        return 0;
+    }
+}
