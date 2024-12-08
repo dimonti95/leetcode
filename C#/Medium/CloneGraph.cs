88f7de75337f7b955d2ddfa6871f7515d7f66861
Nick  DiMonti <dimonti94@gmail.com>
2024-12-07 20:52:26 -0500

    133. Clone Graph

---
+++ C#/Medium/CloneGraph.cs
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+public class Solution {
+    public Node CloneGraph(Node node)
+    {
+        if (node == null) return null;
+
+        var copyMap = new Dictionary<int, Node>();
+
+        Node dfs(Node n)
+        {
+            if (copyMap.ContainsKey(n.val)) return copyMap[n.val];
+            
+            var copy = new Node(n.val);
+            copyMap.Add(n.val, copy);
+
+            foreach (Node neighbor in n.neighbors)
+            {
+                copy.neighbors.Add(dfs(neighbor));
+            }
+
+            return copy;
+        }
+
+        dfs(node);
+
+        return copyMap[node.val];
+    }
+}
+
+/*
+
+    Recursive DFS
+    
+    Time Complexity: O(n + e)
+    Space Compelxity: O(n)
+
+*/
+
+
