2597b5fce9cccdfbb55083e6062f39490dc4c6f4
Nick  DiMonti <dimonti94@gmail.com>
2024-12-09 12:36:44 -0500

    252. Meeting Rooms

---
+++ C#/Easy/MeetingRooms.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public bool CanAttendMeetings(int[][] intervals)
+    {
+        if (intervals.Length == 0) return true;
+
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int[] interval1 = intervals[i - 1];
+            int[] interval2 = intervals[i];
+            if (interval1[1] > interval2[0]) return false;
+        }
+
+        return true;
+    }
+}
+
+/*
+
+    Example 1
+    Input = [[0, 5], [5, 10], [11, 14]]
+    Output = true
+
+    Example 2
+    Input = [[0, 5], [5, 10], [7, 11]]
+    Output = false (because theres overlap between intervals[1] and intervals[2])
+
+    Example 3
+    Input = []
+    Output = true
+
+    Brute force
+    - Compare every combination of intervals
+    - For every interval, one of the following must be true
+        - The start time is greater than or equal to the end time
+        - The end time is less than or equal to the start time
+    
+    Optimization
+    - Sort intervals by start time
+    - Compare end time interval[i] with interval[i + 1]
+    - If the end time > start time, return false, otherwise return true
+    Time: O(nlogn)
+    Space: O(1) (depending on sorting algorithm)
+
+*/
