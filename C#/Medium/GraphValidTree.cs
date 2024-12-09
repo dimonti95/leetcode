34dc1e6b5209ae10ef436c9843199c4b8d9c9a38
Nick  DiMonti <dimonti94@gmail.com>
2024-12-08 21:24:58 -0500

    261. Graph Valid Tree

--- C#/Medium/GraphValidTree.cs
+++ C#/Medium/GraphValidTree.cs
@@ -56,3 +56,66 @@ public class Solution {
     Where v is the number of nodes in the graph and e is the number of edges
 
 */
+
+
+
+public class Solution {
+    public bool ValidTree(int n, int[][] edges)
+    {
+        if (edges.Length != n - 1) return false;
+
+        // Build adjacency list
+        var adjList = new Dictionary<int, List<int>>();
+        for (int i = 0; i < n; i++) adjList.Add(i, new List<int>());
+        foreach (int[] edge in edges)
+        {
+            int n1 = edge[0];
+            int n2 = edge[1];
+
+            // The edges are undirected
+            adjList[n1].Add(n2);
+            adjList[n2].Add(n1);
+        }
+
+        var visited = new HashSet<int>();
+        void dfs(int node)
+        {
+            if (visited.Contains(node)) return;
+            
+            visited.Add(node);
+            foreach (int neighbor in adjList[node])
+            {
+                dfs(neighbor);
+            }
+        }
+
+        dfs(0);
+
+        return visited.Count == n;
+    }
+}
+
+/*
+
+    Recursive DFS + Advanced Graph Theory
+
+    For an undirected graph:
+    - If the graph is connected and contains exactly n−1 edges, it is guaranteed to be a tree
+    - If it has more than n−1 edges, it must contain at least one cycle
+    - If it has fewer than n−1 edges, it will be disconnected and not a tree
+
+    Solution:
+    1. Confirm the graph has exactly n-1 edges
+        - If it has fewer, return false
+        - If it has more, return false
+    2. Build and adjacency list
+    3. Perform DFS and confirm all nodes have been visited (to confirm the graph is connected)
+        - If it is connected, return true
+        - If it's not connected, return false
+
+    Time: O(n) - because we've already confirmed that the number of edges is n-1
+    Space: O(n)
+
+    Where n is the number of nodes
+
+*/
