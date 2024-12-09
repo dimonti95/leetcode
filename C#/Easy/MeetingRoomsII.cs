3669fc2e7ebc7426e99af080e0b8a16756e55815
Nick  DiMonti <dimonti94@gmail.com>
2024-12-09 14:30:07 -0500

    253. Meeting Rooms II

---
+++ C#/Easy/MeetingRoomsII.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int MinMeetingRooms(int[][] intervals)
+    {
+        // Sort intervals by start time
+        Array.Sort(intervals, (a,b) => a[0].CompareTo(b[0]));
+
+        int max = 1;
+        var pQueue = new PriorityQueue<int, int>();
+        pQueue.Enqueue(intervals[0][1], intervals[0][1]);
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int[] interval = intervals[i];
+            int start = interval[0];
+            int end = interval[1];
+            if (start < pQueue.Peek()) pQueue.Enqueue(end, end);
+            else pQueue.DequeueEnqueue(end, end);
+            max = Math.Max(max, pQueue.Count);
+        }
+
+        return max;
+    }
+}
+
+/*
+
+    Solution 2
+    1. Sort the input array by start time
+    2. Use a priority queue to keep track of the meeting that ends first
+        - When finding a meeting that starts before the earliest end time, simply that end time to the priorty queue
+        - When finding a meeting that starts after the earliest end time, replace the end time with the new end time
+    3. Keep a max that tracks the max size that the priority queue grows to
+
+    Time: O(nlogn)
+    Space: O(n)
+
+    Where n is the number of intervals/meetings in the input array
+
+*/
