1c4532fe97e85c8ee264b333a80b82882a59d8fd
Nick  DiMonti <dimonti94@gmail.com>
2024-12-11 15:28:50 -0500

    54. Spiral Matrix

---
+++ C#/Medium/SpiralMatrix.cs
@@ -0,0 +1,60 @@
+public class Solution {
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        int nRows = matrix.Length;
+        int nCols = matrix[0].Length;
+        
+        int top = 0;
+        int bottom = nRows;
+        int left = 0;
+        int right = nCols;
+
+        var result = new List<int>();
+        while (top < bottom && left < right)
+        {
+            // Right
+            for (int i = left; i < right; i++)
+            {
+                result.Add(matrix[top][i]);
+            }
+            top++;
+
+            // Down
+            for (int i = top; i < bottom; i++)
+            {
+                result.Add(matrix[i][right - 1]);
+            }
+            right--;
+
+            if (top >= bottom || left >= right) 
+            {
+                break;
+            }
+
+            // Left
+            for (int i = right - 1; i >= left; i--)
+            {
+                result.Add(matrix[bottom - 1][i]);
+            }
+            bottom--;
+
+            // Up
+            for (int i = bottom - 1; i >= top; i--)
+            {
+                result.Add(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return result;
+    }
+}
+
+/*
+
+    Keep pointers at the bounds of the matrix
+
+    Time: O(m*n)
+    Space: O(1)
+
+*/
