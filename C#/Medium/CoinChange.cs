a289a0410b24ed0f24b40581bfbfeae04a596174
Nick  DiMonti <dimonti94@gmail.com>
2024-12-13 11:45:48 -0500

    322. Coin Change

---
+++ C#/Medium/CoinChange.cs
@@ -0,0 +1,41 @@
+public class Solution
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        var memo = new Dictionary<int, int>();
+
+        int CoinChangeRecursive(int[] coins, int amount)
+        {
+            // Base case
+            if (amount < 0) return -1;
+            if (amount == 0) return 0;
+            if (memo.ContainsKey(amount)) return memo[amount];
+
+            int min = Int32.MaxValue;
+            foreach (int coin in coins)
+            {
+                int result = CoinChangeRecursive(coins, amount - coin);
+                if (result != -1)
+                {
+                    min = Math.Min(min, result);
+                }
+            }
+
+            memo[amount] = min == Int32.MaxValue ? -1 : min + 1;
+            return memo[amount];
+        }
+
+        return CoinChangeRecursive(coins, amount);
+    }
+}
+
+/*
+
+    Top-down DP (memoization)
+
+    Time: O(a * c)
+    Space: O(a)
+
+    Where a is the amount and c is the denomination count (the size of coins)
+
+*/
