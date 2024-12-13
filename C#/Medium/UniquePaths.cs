7c680c0ff62167b26a0296411c231a50ea008ac5
Nick  DiMonti <dimonti94@gmail.com>
2024-12-13 18:38:49 -0500

    62. Unique Paths

---
+++ C#/Medium/UniquePaths.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public int UniquePaths(int m, int n)
+    {
+        var memo = new int[m + 1][];
+        for (int i = 0; i <= m; i++)
+        {
+            memo[i] = new int[n + 1];
+        }
+
+        int UniquePathsRecursive(int m, int n)
+        {
+            if (m == 1 && n == 1) return 1;
+            if (m < 1 || n < 1) return 0;
+            if (memo[m][n] > 0) return memo[m][n];
+
+            memo[m][n] = UniquePathsRecursive(m - 1, n) + UniquePathsRecursive(m, n - 1);
+            return memo[m][n];
+        }
+
+        return UniquePathsRecursive(m, n);        
+    }
+}
+
+/*
+
+    Top-down memoization
+
+    Time: O(m*n)
+    Space: O(m*n)
+
+*/
