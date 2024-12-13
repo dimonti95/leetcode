b7680b90afbf92ba1e969d2fd3e9748b0a6bcc7d
Nick  DiMonti <dimonti94@gmail.com>
2024-12-12 20:45:16 -0500

    70. Climbing Stairs

---
+++ C#/Easy/ClimbingStairs.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 0 || n == 1) return 1;
+
+        return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+    }
+}
+
+/*
+
+    Brute force
+    * Try all possible combinations
+
+    Time: O(2^n)
+    Space: O(n)
+
+*/
