9393c171060e6ed99c0bb01349ee09022947a4dd
Nick  DiMonti <dimonti94@gmail.com>
2024-12-12 20:57:35 -0500

    70. Climbing Stairs

--- C#/Easy/ClimbingStairs.cs
+++ C#/Easy/ClimbingStairs.cs
@@ -10,10 +10,39 @@ public class Solution
 
 /*
 
-    Brute force
+    Brute force recursion
     * Try all possible combinations
 
     Time: O(2^n)
     Space: O(n) because of the depth of the call stack (recursion tree)
+
+*/
+
+
+
+public class Solution {
+    public int ClimbStairs(int n)
+    {
+        var memo = new int[n + 1];
+
+        int Recurse(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            if (memo[n] > 0) return memo[n];
+            
+            memo[n] = Recurse(n - 1) + Recurse(n - 2);
+            return memo[n];
+        }
+
+        return Recurse(n);
+    }
+}
+
+/*
+
+    Memoized recursion
+
+    Time: O(n)
+    Space: O(n)
 
 */
