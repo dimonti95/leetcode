20779a927e14303c3d5033d6c411cba49b2c773e
Nick  DiMonti <dimonti94@gmail.com>
2024-12-14 11:16:43 -0500

    55. Jump Game

--- C#/Medium/JumpGame.cs
+++ C#/Medium/JumpGame.cs
@@ -1,4 +1,5 @@
-public class Solution {
+public class Solution
+{
     public bool CanJump(int[] nums)
     {
         var memo = new Dictionary<int, bool>();
@@ -33,5 +34,75 @@ public class Solution {
     Space: O(n)
 
     Where n is the length of the input array
+
+*/
+
+
+
+public class Solution2
+{
+    public bool CanJump(int[] nums)
+    {
+        var table = new bool[nums.Length];
+        table[table.Length - 1] = true;
+        int lastTrueIndex = table.Length - 1;
+
+        for (int i = table.Length - 2; i >= 0; i--)
+        {
+            int distance = lastTrueIndex - i;
+            if (nums[i] >= distance)
+            {
+                table[i] = true;
+                lastTrueIndex = i;
+            }
+        }
+
+        return table[0];
+    }
+}
+
+/*
+    
+    Bottom-up DP (tabulation)
+    * Create a new array of bools where each value represents whether or not the last index can be reached
+    * The last index will always be true, this is the base case (smallest sub-problem)
+    * Iterate, starting from the back of nums/table 
+    * If n - 1 index can reach the next closest "true index" then it is also true, otherwise false
+
+    Time: O(n)
+    Space: O(n)
+
+    Example
+
+    Input: nums=[2,3,4,0,0,1,2]
+    Output: true
+
+    i = -  nums  = [2,3,4,0,0,1,2]
+           table = [F,F,F,F,F,F,T]
+                                ^
+
+    i = 5  nums  = [2,3,4,0,0,1,2]
+           table = [F,F,F,F,F,T,T]  1 >= (6-5) = T
+                              ^ ^
+
+    i = 4  nums  = [2,3,4,0,0,1,2]
+           table = [F,F,F,F,F,T,T]  0 >= (5-4) = F
+                            ^ ^
+
+    i = 3  nums  = [2,3,4,0,0,1,2]
+           table = [F,F,F,F,F,T,T]  0 >= (5-3) = F
+                          ^   ^
+
+    i = 2  nums  = [2,3,4,0,0,1,2]
+           table = [F,F,T,F,F,T,T]  4 >= (5-2) = T
+                        ^     ^
+    
+    i = 1  nums  = [2,3,4,0,0,1,2]
+           table = [F,T,T,F,F,T,T]  3 >= (2-1) = T
+                      ^ ^    
+
+    i = 0  nums  = [2,3,4,0,0,1,2]
+           table = [T,T,T,F,F,T,T]  2 >= (1-0) = T
+                    ^ ^        
 
 */
