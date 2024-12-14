747b9c72301881ef54b768bcab2600444e65563a
Nick  DiMonti <dimonti94@gmail.com>
2024-12-13 21:15:22 -0500

    62. Unique Paths

--- C#/Medium/UniquePaths.cs
+++ C#/Medium/UniquePaths.cs
@@ -10,7 +10,7 @@ public class Solution
 
         int UniquePathsRecursive(int m, int n)
         {
-            if (m == 1 && n == 1) return 1;
+            if (m == 1 || n == 1) return 1;
             if (m < 1 || n < 1) return 0;
             if (memo[m][n] > 0) return memo[m][n];
 
