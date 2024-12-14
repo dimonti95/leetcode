af2e2c0932c7de112071fa383c43d817e7ea83f1
Nick  DiMonti <dimonti94@gmail.com>
2024-12-13 23:10:19 -0500

    55. Jump Game

---
+++ C#/Medium/JumpGame.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public bool CanJump(int[] nums)
+    {
+        var memo = new Dictionary<int, bool>();
+
+        bool CanJump(int num)
+        {
+            if (num == nums.Length - 1) return true;
+            if (num >= nums.Length) return false;
+            if (memo.ContainsKey(num)) return memo[num];
+
+            for (int i = 1; i <= nums[num]; i++)
+            {
+                if(CanJump(num + i))
+                {
+                    memo[num] = true;
+                    return memo[num];
+                }
+            }
+
+            memo[num] = false;
+            return memo[num];
+        }
+
+        return CanJump(0);
+    }
+}
+
+/*
+
+    Top-down DP (memoization)
+
+    Time: O(n^2)
+    Space: O(n)
+
+    Where n is the length of the input array
+
+*/
