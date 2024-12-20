334dfbdc4eaecd8783bb3169b7f1b46e2243308f
Nick  DiMonti <dimonti94@gmail.com>
2024-12-20 10:29:34 -0500

    191. Number of 1 Bits

--- C#/Easy/NumberOf1Bits.cs
+++ C#/Easy/NumberOf1Bits.cs
@@ -6,7 +6,7 @@ public class Solution
         int count = 0;
         for (int i = 0; i < 32; i++)
         {
-            if ((n & mask) > 0) count++;
+            if ((n & mask) != 0) count++;
             mask = mask << 1;
         }
 
