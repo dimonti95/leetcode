5ec506eaf96e5a7822cb3686dc40311b05654dcd
Nick  DiMonti <dimonti94@gmail.com>
2024-12-21 15:20:08 -0500

    268. Missing Number

---
+++ C#/Easy/ReverseBits.cs
@@ -0,0 +1,30 @@
+public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        uint result = 0;
+        uint rMask = 1U << 31;
+        while (n > 0)
+        {
+            // check LSB is 1
+            if ((n & 1) > 0)
+            {
+                result ^= rMask;
+            }
+
+            n >>>= 1;
+            rMask >>>= 1;
+        }
+
+        return result;
+    }
+}
+
+/*
+
+    Compare each bit
+
+    Time: O(1)
+    Space: O(1)
+
+*/
