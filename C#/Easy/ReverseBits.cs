09f30db37111a17c63c6fd59ae8072c1c15478e9
Nick  DiMonti <dimonti94@gmail.com>
2024-12-22 18:51:16 -0500

    190. Reverse Bits

--- C#/Easy/ReverseBits.cs
+++ C#/Easy/ReverseBits.cs
@@ -82,3 +82,74 @@ public class Solution2
     Space: O(1) because the memoization cache will grow (at most) to hold 256 bytes
 
 */
+
+
+
+public class Solution3
+{
+    public uint reverseBits(uint n)
+    {
+        // swap each 16-bit side of the 32-bit uint
+        n = (n >>> 16) | (n << 16);
+
+        // swap each 8-bit pair
+        n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+
+        // swap each 4-bit pair
+        n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+
+        // swap each 2-bit pair
+        n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+
+        // swap each bit
+        n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+
+        return n;
+    }
+}
+
+/*
+
+    Solution 3
+
+    Divide and conquer
+
+    Response to the following follow-up question: How would you solve it without using a loop?
+
+    Time: O(1)
+    Space: O(1)
+
+    Example
+    Input: n = 0000 0010 1001 0100 0001 1110 1001 1100
+    Output: 0011 1001 0111 1000 0010 1001 0100 0000
+
+    Left bit masks:
+    1111 1111 0000 0000 1111 1111 0000 0000 = 0xff00ff00
+    1111 0000 1111 0000 1111 0000 1111 0000 = 0xf0f0f0f0
+    1100 1100 1100 1100 1100 1100 1100 1100 = 0xcccccccc
+    1010 1010 1010 1010 1010 1010 1010 1010 = 0xaaaaaaaa
+    
+    Right bit masks:
+    0000 0000 1111 1111 0000 0000 1111 1111 = 0x00ff00ff
+    0000 1111 0000 1111 0000 1111 0000 1111 = 0x0f0f0f0f
+    0011 0011 0011 0011 0011 0011 0011 0011 = Ox33333333
+    0101 0101 0101 0101 0101 0101 0101 0101 = 0x55555555
+
+    0000 0010 1001 0100 0001 1110 1001 1100
+                     |
+                     v
+    0001 1110 1001 1100 0000 0010 1001 0100         n = (n >>> 16) | (n << 16)
+                     |
+                     v
+    1001 1100 0001 1110 1001 0100 0000 0010         n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8)
+                     |
+                     v
+    1100 1001 1110 0001 0100 1001 0010 0000         n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4)
+                     |
+                     v
+    0011 0110 1011 0100 0001 0110 1000 0000         n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2)
+                     |
+                     v
+    0011 1001 0111 1000 0010 1001 0100 0000         n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1)
+
+*/
