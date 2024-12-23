554089bf65a11040811f1793f9623e9d3c26ed90
Nick  DiMonti <dimonti94@gmail.com>
2024-12-23 11:09:21 -0500

    53. Maximum Subarray

--- C#/Medium/MaximumSubarray.cs
+++ C#/Medium/MaximumSubarray.cs
@@ -1,5 +1,7 @@
-public class Solution {
-    public int MaxSubArray(int[] nums) {
+public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
         int max = nums[0];
         int currentSum = 0;
 
@@ -15,7 +17,7 @@ public class Solution {
 
 /* 
 
-  Explanation:
+  Bottom-up DP (memory optimized)
 
   Loop over the array keeping a current sum to be either the current value or the current sum up to that point
   + the current value, whichever is larger. Then check for a new max.
