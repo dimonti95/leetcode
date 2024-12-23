b26f3ddd54c9f89d81d7a5a2081c47dc5a53bf2c
Nick  DiMonti <dimonti94@gmail.com>
2024-12-23 12:23:26 -0500

    53. Maximum Subarray

--- C#/Medium/MaximumSubarray.cs
+++ C#/Medium/MaximumSubarray.cs
@@ -2,6 +2,58 @@ public class Solution
 {
     public int MaxSubArray(int[] nums)
     {
+        return MaxSubArrayRecursive(0, nums.Length - 1, nums);
+    }
+
+    private int MaxSubArrayRecursive(int left, int right, int[] nums)
+    {
+        if (left > right) return int.MinValue;
+
+        int mid = (left + right) / 2;
+
+        // left sum
+        int count = 0;
+        int leftSum = 0;
+        for (int i = mid - 1; i >= left; i--)
+        {
+            count += nums[i];
+            leftSum = Math.Max(leftSum, count);
+        }
+
+        // right sum
+        count = 0;
+        int rightSum = 0;
+        for (int i = mid + 1; i <= right; i++)
+        {
+            count += nums[i];
+            rightSum = Math.Max(rightSum, count);
+        }
+
+        int leftMax = MaxSubArrayRecursive(left, mid - 1, nums);
+        int rightMax = MaxSubArrayRecursive(mid + 1, right, nums);
+
+        int combinedMax = nums[mid] + leftSum + rightSum;
+        return Math.Max(combinedMax, Math.Max(leftMax, rightMax));
+    }
+}
+
+/*
+
+    Divide and Conquer
+
+    Time: O(nlogn)
+    Space: O(logn)
+
+    Solution for the follow-up question: Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+*/
+
+
+
+public class Solution2
+{
+    public int MaxSubArray(int[] nums)
+    {
         int max = nums[0];
         int currentSum = 0;
 
