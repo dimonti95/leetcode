326b60e38308042cdc09965ba5d2e9ec38f9eebc
Nick  DiMonti <dimonti94@gmail.com>
2025-01-02 13:22:03 -0500

    226. Invert Binary Tree

--- C#/Easy/InvertBinaryTree.cs
+++ C#/Easy/InvertBinaryTree.cs
@@ -35,3 +35,50 @@ public class Solution
     Where n is the number of nodes and h is the height of the tree
 
 */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null) return null;
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            TreeNode current = queue.Dequeue();
+            if (current.left != null) queue.Enqueue(current.left);
+            if (current.right != null) queue.Enqueue(current.right);
+
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+        }
+
+        return root;
+    }
+}
+
+/*
+
+    Iterative BFS (iterative DFS would work too)
+
+    Time: O(n)
+    Space: O(n) because in the worst case all n/2 leaf nodes will be in the queue
+
+*/
