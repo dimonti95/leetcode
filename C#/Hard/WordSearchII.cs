a7eee035650d3ad32979c0b9f1c4a89e65a7b4b7
Nick  DiMonti <dimonti94@gmail.com>
2025-01-04 13:14:09 -0500

    git status

--- C#/Hard/WordSearchII.cs
+++ C#/Hard/WordSearchII.cs
@@ -1,41 +1,41 @@
-public class Solution {
+public class Solution
+{
     public IList<string> FindWords(char[][] board, string[] words)
     {
+        var result = new HashSet<string>();
         var trie = new Trie();
         int nRows = board.Length;
         int nCols = board[0].Length;
+
+        // Add every word to the Trie
         foreach (string word in words)
         {
             trie.AddWord(word);
         }
 
-        var result = new HashSet<string>();
-        var path = new HashSet<string>();
-
-        void dfs(int r, int c, TrieNode node, StringBuilder word)
+        void dfs(int r, int c, TrieNode current, StringBuilder sb)
         {
-            string key = r + "," + c;
-            if (r < 0 || c < 0 || r >= nRows || c >= nCols || path.Contains(key)) return;
-
-            char ch = board[r][c];
-            if (!node.Children.ContainsKey(ch)) return;
+            if (r < 0 || c < 0 || r >= nRows || c >= nCols) return;
             
-            word.Append(ch);
-            if (node.Children[ch].IsWord && !result.Contains(word.ToString())) result.Add(word.ToString());
+            char ch = board[r][c];
+            if (!current.Children.ContainsKey(ch)) return;
 
-            path.Add(key);
-            dfs(r + 1, c, node.Children[ch], word);
-            dfs(r - 1, c, node.Children[ch], word);
-            dfs(r, c + 1, node.Children[ch], word);
-            dfs(r, c - 1, node.Children[ch], word);
-            path.Remove(key);
-            word.Length--;  // Backtrack by removing last character
+            sb.Append(ch);
+            if (current.Children[ch].IsWord && !result.Contains(sb.ToString())) result.Add(sb.ToString());
+
+            board[r][c] = '#';
+            dfs(r + 1, c, current.Children[ch], sb);
+            dfs(r - 1, c, current.Children[ch], sb);
+            dfs(r, c + 1, current.Children[ch], sb);
+            dfs(r, c - 1, current.Children[ch], sb);
+            board[r][c] = ch;
+            sb.Length--;
         }
 
         for (int r = 0; r < nRows; r++)
         {
             for (int c = 0; c < nCols; c++)
-            {  
+            {
                 dfs(r, c, trie.Root, new StringBuilder());
             }
         }
