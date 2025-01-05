70bff76c22e924c1fc98eed55c02510a0ef14709
Nick  DiMonti <dimonti94@gmail.com>
2025-01-05 13:27:40 -0500

    102. Binary Tree Level Order Traversal

--- C#/Medium/BinaryTreeLevelOrderTraversal.cs
+++ C#/Medium/BinaryTreeLevelOrderTraversal.cs
@@ -15,6 +15,51 @@ public class Solution
 {
     public IList<IList<int>> LevelOrder(TreeNode root)
     {
+        var result = new List<IList<int>>();
+        void dfs(TreeNode node, int level)
+        {
+            if (node == null) return;
+
+            if (level == result.Count) result.Add(new List<int>());
+            result[level].Add(node.val);
+
+            dfs(node.left, level + 1);
+            dfs(node.right, level + 1);
+        }
+
+        dfs(root, 0);
+        return result;
+    }
+}
+
+/*
+
+    Recursive DFS
+
+    Time: O(n)
+    Space: O(h)
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
         if (root == null) return new List<IList<int>>();
 
         var res = new List<IList<int>>();
