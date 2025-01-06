06462436d33dc5293b9c85fa257138ca68df680d
Nick  DiMonti <dimonti94@gmail.com>
2025-01-06 18:27:25 -0500

    733. Flood Fill

---
+++ C#/Easy/FloodFill.cs
@@ -0,0 +1,35 @@
+public class Solution
+{
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        int nRows = image.Length;
+        int nCols = image[0].Length;
+        int targetColor = image[sr][sc];
+
+        void dfs(int r, int c)
+        {
+            if (r < 0 || c < 0 || r >= nRows || c >= nCols) return;
+            if (image[r][c] != targetColor || image[r][c] == color) return;
+
+            image[r][c] = color;
+            dfs(r + 1, c);
+            dfs(r - 1, c);
+            dfs(r, c + 1);
+            dfs(r, c - 1);
+        }
+
+        dfs(sr, sc);
+        return image;
+    }
+}
+
+/*
+
+    DFS
+
+    Time: O(m * n)
+    Space: O(m * n)
+
+    Where m is the number of rows and n is the number of columns
+
+*/
