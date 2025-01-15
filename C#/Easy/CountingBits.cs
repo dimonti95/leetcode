afef5d8f0789abaf01d49ad707f518b715fce9d1
Nick  DiMonti <dimonti94@gmail.com>
2025-01-15 11:32:19 -0500

    338. Counting Bits

--- C#/Easy/CountingBits.cs
+++ C#/Easy/CountingBits.cs
@@ -3,22 +3,24 @@ public class Solution1
     public int[] CountBits(int n)
     {
         var result = new int[n + 1];
-        int mask = 1;
-        for (int i = 0; i <= n; i++)
-        {   
-            int x = i;
-            int count = 0;
-
-            // This is O(logn) since this number is being divided by 2
-            while (x != 0)
-            {
-                if ((x & mask) != 0) count++;
-                x >>= 1; // this is the divide by 2
-            }
-
-            result[i] = count;
+        for (int i = 1; i <= n; i++)
+        {
+            result[i] = Count(i);
         }
-        
+
+        return result;
+    }
+
+    // This can be said to be O(logn) since this number is being divided by 2
+    private int Count(int n)
+    {
+        int result = 0;
+        while (n > 0)
+        {
+            n &= (n - 1); // removes the LSB each iteration
+            result++;
+        }
+
         return result;
     }
 }
