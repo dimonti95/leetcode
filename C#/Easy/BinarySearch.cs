e0df1cef54d8071168415e6e10fc9a6f4c20e9ed
Nick  DiMonti <dimonti94@gmail.com>
2025-01-17 18:10:45 -0500

    704. Binary Search

--- C#/Easy/BinarySearch.cs
+++ C#/Easy/BinarySearch.cs
@@ -33,3 +33,23 @@ public class Solution
     Space: O(1)
 
 */
+
+
+
+public class Solution2
+{
+    public int Search(int[] nums, int target)
+    {
+        int i = Array.BinarySearch(nums, target);
+        return i < 0 ? -1 : i;
+    }
+}
+
+/*
+    
+    Solution 2: Use a built in function.
+
+    Time: O(logn)
+    Space: O(1)
+
+*/
