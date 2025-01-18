29171d13d81bdbde5ce0ded4aff30a4a69b2b39a
Nick  DiMonti <dimonti94@gmail.com>
2025-01-18 12:18:23 -0500

    383. Ransom Note

--- C#/Easy/RansomNote.cs
+++ C#/Easy/RansomNote.cs
@@ -22,7 +22,21 @@ public class Solution
 
 /*
 
-    Use a single hash map to track character count
+    Brute force solution:
+    * For each character found in ransomNote, find and remove that character in magazine
+    * If the character is not in magazine, then return false
+    * Otherwise, if all ransomNote characters have been removed from magazine, return true
+
+    This is the most straighforward approach, but because strings are immmutable (can't be modified), it's not optimal.
+
+    This is because we would basically need to replace magazine with a new string for each character removed.
+
+    Time: O(n*m)
+    Space: O(m) - because of the new magazine object being created
+
+    --------------------------------------------------
+
+    Optimized Solution: Use a single hash map to track character count
 
     Time: O(n + m)
     Space: O(26) = O(1)
