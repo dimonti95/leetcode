60291cb62b838dd2b524240eb93ad5e05cf8b4e4
Nick  DiMonti <dimonti94@gmail.com>
2025-01-24 18:43:27 -0500

    20. Valid Parentheses

--- Swift/Easy/ValidParentheses.swift
+++ Swift/Easy/ValidParentheses.swift
@@ -28,3 +28,37 @@ class Solution {
     Where n is the length of the input string
 
 */
+
+
+
+class Solution2 {
+    func isValid(_ s: String) -> Bool {
+        var map = [Character: Character]()
+        var stack = [Character]()
+        map["("] = ")"
+        map["["] = "]"
+        map["{"] = "}"
+
+        for char in s {
+            if let temp = map[char] {
+                stack.append(temp)
+                continue
+            }
+
+            if stack.isEmpty { return false }
+            let last = stack.removeLast()
+            if last != char { return false }
+        }
+
+        return stack.isEmpty
+    }
+}
+
+/*
+
+    Time: O(n)
+    Space: O(n)
+
+    Same solution as the first but the mappings are stored in a dictionary
+
+*/
