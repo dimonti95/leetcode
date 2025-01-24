2d64becde0102cecd23723927bdf9e57dc76dc31
Nick  DiMonti <dimonti94@gmail.com>
2025-01-23 20:41:08 -0500

    1. Two Sum

--- Swift/Easy/TwoSum.swift
+++ Swift/Easy/TwoSum.swift
@@ -1,7 +1,5 @@
-class Solution
-{
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int]
-    {
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var map: [Int:Int] = [:]
         for i in 0..<nums.count
         {
