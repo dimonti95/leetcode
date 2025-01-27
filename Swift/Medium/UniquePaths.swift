233176c9c028efececaa88180054360ed2e48f71
Nick  DiMonti <dimonti94@gmail.com>
2025-01-26 19:57:39 -0500

    62. Unique Paths

--- Swift/Medium/UniquePaths.swift
+++ Swift/Medium/UniquePaths.swift
@@ -22,3 +22,33 @@ class Solution {
     Space: O(m * n)
 
 */
+
+
+
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var table = Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1)
+
+        for r in 0..<m {
+            for c in 0..<n {
+                if r == 0 || c == 0 {
+                    table[r][c] = 1
+                }
+                else {
+                    table[r][c] = table[r - 1][c] + table[r][c - 1]
+                }
+            }
+        }
+
+        return table[m - 1][n - 1]
+    }
+}
+
+/*
+
+    Bottom-up DP
+
+    Time: O(m * n)
+    Space: O(m * n)
+
+*/
