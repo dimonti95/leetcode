31e0ec35ba1ba2a46a17e765e8cb6ca61dc09640
Nick  DiMonti <dimonti94@gmail.com>
2025-01-27 21:36:33 -0500

    300. Longest Increasing Subsequence

--- Swift/Medium/LongestIncreasingSubsequence.swift
+++ Swift/Medium/LongestIncreasingSubsequence.swift
@@ -2,16 +2,15 @@ class Solution {
     func lengthOfLIS(_ nums: [Int]) -> Int {
         var memo = Array<Int>(repeating: -1, count: nums.count)
 
-        func dp(_ index: Int, _ sequence: [Int]) -> Int {
+        func dp(_ index: Int, _ sequence: inout [Int]) -> Int {
             if (index == nums.count) { return 0 }
             if (memo[index] != -1) { return memo[index] }
 
-            var sequence = sequence
             var currentMax = 0
             for i in index..<nums.count {
                 if sequence.count == 0 || nums[i] > sequence[sequence.count - 1] {
                     sequence.append(nums[i])
-                    currentMax = max(currentMax, 1 + dp(i + 1, sequence))
+                    currentMax = max(currentMax, 1 + dp(i + 1, &sequence))
                     sequence.removeLast()
                 }
             }
@@ -20,7 +19,8 @@ class Solution {
             return memo[index]
         }
 
-        return dp(0, [Int]())
+        var sequence = [Int]()
+        return dp(0, &sequence)
     }
 }
 
@@ -29,7 +29,9 @@ class Solution {
     Top-down DP
 
     Time: O(n^2)
-    Space: O(n)    
+    Space: O(n)
+
+    Note: Using inout keyword so that the array is passed by reference, this avoid creating copies of the array
 
 */
 
