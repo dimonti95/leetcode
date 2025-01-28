3a17456aa153cf01dd68864d9c654bed703ec194
Nick  DiMonti <dimonti94@gmail.com>
2025-01-27 21:14:57 -0500

    300. Longest Increasing Subsequence

--- Swift/Medium/LongestIncreasingSubsequence.swift
+++ Swift/Medium/LongestIncreasingSubsequence.swift
@@ -32,3 +32,35 @@ class Solution {
     Space: O(n)    
 
 */
+
+
+
+class Solution {
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        var dp = Array<Int>(repeating: 1, count: nums.count)
+
+        for i in 1..<nums.count {
+            for j in 0..<i {
+                if nums[i] > nums[j] {
+                    dp[i] = max(dp[i], dp[j] + 1)
+                }
+            }
+        }
+
+        var longest = 0
+        for val in dp {
+            longest = max(longest, val)
+        }
+
+        return longest
+    }
+}
+
+/*
+
+    Bottom-up DP
+
+    Time: O(n^2)
+    Space: O(n)
+
+*/
